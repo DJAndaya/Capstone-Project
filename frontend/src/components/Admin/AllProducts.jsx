04ea78d2c59059e5ad85b15930dcd5e5440b10e8
Elia Lee <elia.lee1984@gmail.com>
2023-11-19 23:44:49 +0100

    added pagination to AllProducts

--- frontend/src/components/Admin/AllProducts.jsx
+++ frontend/src/components/Admin/AllProducts.jsx
@@ -1,10 +1,15 @@
 import React, { useState, useEffect } from "react";
 import TemporaryDrawer from "./Drawer";
 
+const itemsPerPage = 20;
+
 export default function AllProducts() {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+  const [pageInput, setPageInput] = useState("");
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -16,6 +21,7 @@ export default function AllProducts() {
 
         const data = await response.json();
         setProducts(data);
+        calculateTotalPages(data.length);
         setLoading(false);
       } catch (error) {
         setError(error.message);
@@ -26,6 +32,28 @@ export default function AllProducts() {
     fetchProducts();
   }, []);
 
+  const calculateTotalPages = (totalItems) => {
+    setTotalPages(Math.ceil(totalItems / itemsPerPage));
+  };
+
+  const handlePageChange = (newPage) => {
+    setCurrentPage(newPage);
+  };
+
+  const handleManualPageChange = () => {
+    const pageNumber = parseInt(pageInput, 10);
+    if (pageNumber > 0 && pageNumber <= totalPages) {
+      setCurrentPage(pageNumber);
+      setPageInput("");
+    }
+  };
+
+  const renderProductsForCurrentPage = () => {
+    const startIndex = (currentPage - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+    return products.slice(startIndex, endIndex);
+  };
+
   return (
     <div>
       <TemporaryDrawer />
@@ -35,10 +63,44 @@ export default function AllProducts() {
       {error && <p>Error: {error}</p>}
 
       <ul>
-        {products.map((product) => (
-          <li key={product.id}>{product.name}</li>
+        {renderProductsForCurrentPage().map((product) => (
+          <li key={product.id}>
+            <p>Product: {product.name}</p>
+            <p>Price: ${parseFloat(product.price).toFixed(2)}</p>
+            <p>Description: {product.description}</p>
+          </li>
         ))}
       </ul>
+
+      <div>
+        <p>
+          Page {currentPage} of {totalPages}
+        </p>
+        <button
+          onClick={() => handlePageChange(currentPage - 1)}
+          disabled={currentPage === 1}
+        >
+          Previous Page
+        </button>
+
+        <span>
+          Go to Page:{" "}
+          <input
+            type="number"
+            value={pageInput}
+            onChange={(e) => setPageInput(e.target.value)}
+            min="1"
+            max={totalPages}
+          />
+          <button onClick={handleManualPageChange}>Go</button>
+          <button
+            onClick={() => handlePageChange(currentPage + 1)}
+            disabled={currentPage === totalPages}
+          >
+            Next Page
+          </button>
+        </span>
+      </div>
     </div>
   );
 }
