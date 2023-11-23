6e97c9e7aa357133174d58ad98c61e0486e16fec
Darell Andaya <dje.andaya@gmail.com>
2023-11-22 17:44:49 -0800

    can add to cart without sign in, cleaned and deleted unused code

--- frontend/src/components/AddToCartButton.jsx
+++ frontend/src/components/AddToCartButton.jsx
@@ -1,91 +1,29 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
 // redux
-import { useSelector, useDispatch } from "react-redux";
-import { updateShoppingCart } from "../redux/shoppingCartSlice";
+import { useSelector } from "react-redux";
 // router
-import { useNavigate, useLocation, useOutletContext } from "react-router-dom";
+import { useOutletContext } from "react-router-dom";
 // components
 import { Button } from "@mui/material";
 
 const AddToCartButton = ({ item }) => {
-  // const [shoppingCart, setShoppingCart] = useState([]);
-  const [itemInShop, setItemInShop] = useState(false);
-
   const userId = useSelector((state) => state.isAuth?.value?.id);
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
-  const location = useLocation();
 
   const [shoppingCart, setShoppingCart] = useOutletContext();
   let isItemInShoppingCart = shoppingCart.some(
     (cartItem) => cartItem.id === item.id
   );
 
-  useEffect(() => {
-    const getShoppingCartData = async () => {
-      const shoppingCartResponse = await axios.get(
-        `http://localhost:3000/items/shoppingCart/`,
-        {
-          params: { userId: userId },
-        }
-      );
-      const alphabeticalOrderData = shoppingCartResponse.data.sort((a, b) => {
-        return a.name > b.name ? 1 : -1;
+  const addRemoveFromShoppingCart = () => {
+    if (isItemInShoppingCart) {
+      const updatedShoppingCart = shoppingCart.filter((shoppingCartItem) => {
+        return shoppingCartItem.id !== item.id;
       });
-      const isItemInShoppingCart = alphabeticalOrderData.some(
-        (cartItem) => cartItem.id === item.id
-      );
-      setItemInShop(isItemInShoppingCart);
-      setShoppingCart(alphabeticalOrderData);
-    };
-  }, [item, userId, location.pathname]);
-
-  const addRemoveFromShoppingCart = async () => {
-    if (!userId) {
-      navigate("/login");
-      return;
-    }
-
-    const patchData = { item, userId };
-    try {
-      const response = await axios.patch(
-        "http://localhost:3000/items/addOrRemoveFromShoppingCart",
-        patchData
-      );
-
-      const token = response.data;
-      window.localStorage.setItem("token", token);
-
-      const shoppingCartResponse = await axios.get(
-        `http://localhost:3000/items/shoppingCart/`,
-        {
-          params: { userId: userId },
-        }
-      );
-      const alphabeticalOrderData = shoppingCartResponse.data.sort((a, b) => {
-        return a.name > b.name ? 1 : -1;
-      });
-
-      const isItemInShoppingCart = alphabeticalOrderData.some(
-        (cartItem) => cartItem.id === item.id
-      );
-      setItemInShop(isItemInShoppingCart);
-      setShoppingCart(alphabeticalOrderData);
-      // localStorage.setItem("shoppingCart", JSON.stringify(alphabeticalOrderData));
-      itemInCartCheck();
-    } catch (error) {
-      console.log(error);
+      setShoppingCart(updatedShoppingCart);
+    } else {
+      setShoppingCart([...shoppingCart, item]);
     }
   };
-
-  const itemInCartCheck = () => {
-    isItemInShoppingCart = shoppingCart.some(
-      (cartItem) => cartItem.id === item.id
-    );
-    // setItemInShop(isItemInShoppingCart);
-  };
-  //   itemInCartCheck()
 
   return (
     <>
@@ -107,17 +45,3 @@ const AddToCartButton = ({ item }) => {
 };
 
 export default AddToCartButton;
-
-/* Plans to fix button issue
-
-1. use outlet context to create a state, the state will store array of items
-2. make a variable with .find if the item.id matches an item in the state
-3. the button either removes or adds the item from the array depending on if it's a remove or add 
-
-*/
-
-/* Changes needed to fix 
-
-
-
-*/
