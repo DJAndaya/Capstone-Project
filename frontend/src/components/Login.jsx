c1c8068c5377f199c67f791ec413cc91017a1a78
Patrick Lam <patricklam12345@gmail.com>
2023-12-07 00:15:42 -0500

    basic css styling

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,4 +1,4 @@
- import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 import { setIsAuth } from "../redux/isAuthSlice";
@@ -14,9 +14,11 @@ const Login = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const [outletContext, setOutletContext] = useOutletContext();
-  const wishlist = outletContext.wishlist
-  const shoppingCartItems = outletContext.shoppingCart.map(object => object.item)
-  const shoppingCart = outletContext.shoppingCart
+  const wishlist = outletContext.wishlist;
+  const shoppingCartItems = outletContext.shoppingCart.map(
+    (object) => object.item
+  );
+  const shoppingCart = outletContext.shoppingCart;
   const userId = useSelector((state) => state.isAuth?.value?.id);
 
   const loginUser = async (formData) => {
@@ -38,23 +40,25 @@ const Login = () => {
           },
         }
       );
-        
+
       const user = userResponse.data;
       dispatch(setIsAuth(user));
 
       // putting DB wishlist and shopping cart into local respectively
-      const userWishlist = user.Wishlist || []
-      const userShoppingCart = user.shoppingCart || []
-      const combinedWishlist = wishlist.concat(userWishlist.filter(item => wishlist.indexOf(item) === -1))
+      const userWishlist = user.Wishlist || [];
+      const userShoppingCart = user.shoppingCart || [];
+      const combinedWishlist = wishlist.concat(
+        userWishlist.filter((item) => wishlist.indexOf(item) === -1)
+      );
       const combineShoppingCart = shoppingCart.concat(
         userShoppingCart
-        .filter(item => !shoppingCartItems.includes(item))
-        .map(newItem => ({ item: newItem, amount: 1 }))
-      )
+          .filter((item) => !shoppingCartItems.includes(item))
+          .map((newItem) => ({ item: newItem, amount: 1 }))
+      );
       setOutletContext({
         wishlist: combinedWishlist,
         shoppingCart: combineShoppingCart,
-      })
+      });
 
       // updating user with new wishlist and shopping cart
       const updateUserResponse = await axios.patch(
@@ -64,13 +68,13 @@ const Login = () => {
           shoppingCart: combineShoppingCart,
         },
         {
-          params: {userId: user.id}
+          params: { userId: user.id },
         }
-      )
-      const updatedUser = updateUserResponse.data
+      );
+      const updatedUser = updateUserResponse.data;
       // console.log(updatedUser)
       // console.log("before updatedUserData is put onto local")
-      dispatch(setIsAuth(updatedUser))
+      dispatch(setIsAuth(updatedUser));
     } catch (error) {
       console.log(error);
     }
@@ -80,9 +84,9 @@ const Login = () => {
     if (userId) {
       // console.log("before navigate in useEffect");
       navigate("/");
-      // console.log("after navigate in useEffect");  
+      // console.log("after navigate in useEffect");
     }
-    }, [userId, navigate]);
+  }, [userId, navigate]);
 
   const onSubmit = (event) => {
     event.preventDefault();
@@ -99,9 +103,26 @@ const Login = () => {
   };
 
   return (
-    <>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+        marginTop: "-80px",
+      }}
+    >
       <h1>Login</h1>
-      <form onSubmit={onSubmit}>
+      <form
+        onSubmit={onSubmit}
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          gap: "10px",
+        }}
+      >
         <input
           placeholder="Email"
           value={formData.email}
@@ -128,7 +149,7 @@ const Login = () => {
         </NavLink>
         <br />
       </div>
-    </>
+    </div>
   );
 };
 
